6f3e24884a2c05f4d18744017836261d1e384f99
dzmitry-duboyski <dzmitry.duboyski@gmail.com>
2024-11-19 18:58:11 +0400

    Revert "refactor"
    This reverts commit 2a840b254b8bd365b1f689c12619e85947751d57.

---
+++ TwoCaptcha/Captcha/HCaptcha.cs
@@ -0,0 +1,25 @@
+namespace TwoCaptcha.Captcha
+{
+    public class HCaptcha : Captcha
+    {
+        public HCaptcha() : base()
+        {
+            parameters["method"] = "hcaptcha";
+        }
+
+        public void SetSiteKey(string siteKey)
+        {
+            parameters["sitekey"] = siteKey;
+        }
+
+        public void SetUrl(string url)
+        {
+            parameters["pageurl"] = url;
+        }
+
+        public void SetData(string data)
+        {
+            parameters["data"] = data;
+        }
+    }
+}
