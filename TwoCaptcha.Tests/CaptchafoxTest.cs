f352b6d1c0bfaa9e788957ae9e5aa22840f7f318
victorkowalski <victorkowalski@yandex.ru>
2025-12-03 10:13:20 +0300

    captchafox test upd

--- TwoCaptcha.Tests/CaptchafoxTest.cs
+++ TwoCaptcha.Tests/CaptchafoxTest.cs
@@ -15,13 +15,15 @@ namespace TwoCaptcha.Tests
             captcha.SetSiteKey("sk_ILKWNruBBVKDOM7dZs59KHnDLEWiH");
             captcha.SetUrl("https://mysite.com/page/with/captchafox");
             captcha.SetUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36");
-            captcha.SetProxy("HTTPS", "login:password@IP_address:PORT");
+            captcha.SetProxy("HTTPS", "username:password@1.2.3.4:5678");
 
             var parameters = new Dictionary<string, string>();
             parameters["method"] = "captchafox";
             parameters["sitekey"] = "sk_ILKWNruBBVKDOM7dZs59KHnDLEWiH";
-            parameters["pageurl"] = "\"https://mysite.com/page/with/captchafox";
+            parameters["pageurl"] = "https://mysite.com/page/with/captchafox";
             parameters["userAgent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36";
+            parameters["proxy"] = "username:password@1.2.3.4:5678";
+            parameters["proxytype"] = "HTTPS";
             parameters["soft_id"] = "4582";
 
             await CheckIfCorrectParamsSendAndResultReturned(captcha, parameters);
