fd6b66a5ce3e513d8d5f1571d38ec2ddc0f315b9
victorkowalski <victorkowalski@yandex.ru>
2026-03-30 08:52:09 +0300

    SetApiServer

--- TwoCaptcha.Examples/CaptchafoxExample.cs
+++ TwoCaptcha.Examples/CaptchafoxExample.cs
@@ -15,6 +15,7 @@ namespace TwoCaptcha.Examples
             captcha.SetUrl("https://mysite.com/page/with/captchafox");
             captcha.SetUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36");
             captcha.SetProxy("HTTPS", "login:password@IP_address:PORT");
+            captcha.SetApiServer("api_server");
 
             try
             {
