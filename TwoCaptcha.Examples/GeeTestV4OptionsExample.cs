8fe99193f99f629b71a2a423faacb14c82562f4e
Maxim S <poplers24@gmail.com>
2026-05-27 17:44:22 +0200

    fix geetest v4 example risk_type

--- TwoCaptcha.Examples/GeeTestV4OptionsExample.cs
+++ TwoCaptcha.Examples/GeeTestV4OptionsExample.cs
@@ -14,6 +14,7 @@ namespace TwoCaptcha.Examples
             captcha.SetCaptchaId("72bf15796d0b69c43867452fea615052");
             captcha.SetChallenge("12345678abc90123d45678ef90123a456b");
             captcha.SetUrl("https://2captcha.com/demo/geetest-v4");
+            captcha.SetRiskType("slide|1779896203.529354|33e0e11720904b2aa05ed6f22f0b5132|682a4c4b734094e843928b5b112de1a0aa1e7546fc1feec6f7568c113f6060ec");
             captcha.SetProxy("HTTPS", "login:password@IP_address:PORT");
 
             try
