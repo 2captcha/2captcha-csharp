0b06d40467add0b4e1688bc6ddd61ce847a930c9
victorkowalski <vkowalski@yandex.ru>
2026-07-22 15:47:33 +0300

    base64 example

--- TwoCaptcha.Examples/GenericCaptchaExample.cs
+++ TwoCaptcha.Examples/GenericCaptchaExample.cs
@@ -8,12 +8,22 @@ namespace TwoCaptcha.Examples
     {
         public GenericCaptchaExample(string apiKey)
         {
+            //Text captcha example
             GenericTwoCaptcha solver = new GenericTwoCaptcha(apiKey);
-
+            
             GenericCaptcha captcha = new GenericCaptcha();
             captcha.parameters["method"] = "post";
             captcha.parameters["textcaptcha"] = "If tomorrow is Saturday, what day is today?";
             captcha.parameters["lang"] = "en";
+            
+            //Normal captcha example
+            /*
+            GenericCaptcha captcha = new GenericCaptcha();
+            captcha.parameters["method"] = "base64";
+            byte[] bytes = System.IO.File.ReadAllBytes("resources/normal.jpg");
+            string base64EncodedImage = Convert.ToBase64String(bytes);
+            captcha.parameters["body"] = base64EncodedImage;
+            */
 
             try
             {
